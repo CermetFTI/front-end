9b3297cc23a6c0d5357f2f1f0c3a976e138efc26
Azhar Ikhtiarudin <azharikhtiarudin@gmail.com>
2021-04-22 12:04:31 +0700

    fix card style

--- cermet-fronted/src/components/data.js
+++ cermet-fronted/src/components/data.js
@@ -3,6 +3,22 @@ const data=[
     {
         title: 'Battle of Article',
         image:'https://i.ibb.co/Sr67847/image-1.png',
+        start: 'January 01 2021',
+        end: 'January 02 2022',
+        desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+        link:'https://www.google.com'
+    },
+    {
+        title: 'SejutaCita Youth Inovation Challenge',
+        image:'https://i.ibb.co/bzcq98w/image-3.png',
+        start: 'January 01',
+        end: 'January 01',
+        desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+        link:'https://www.google.com'
+    },
+    {
+        title: 'ICStar Hackathon',
+        image:'https://i.ibb.co/10NNVLz/image-2.png',
         start: 'January 01',
         end: 'January 01',
         desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
