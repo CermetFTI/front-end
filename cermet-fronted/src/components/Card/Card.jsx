740fe82c1b33f29bc855e9851d68bd471cef8d75
MSMazaya <msulthanmazaya@gmail.com>
2021-04-26 19:57:43 +0700

    adjust

--- cermet-fronted/src/components/Card/Card.jsx
+++ cermet-fronted/src/components/Card/Card.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Card.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
-import { faWindows } from '@fortawesome/free-brands-svg-icons';
+import FsLightbox from 'fslightbox-react';
 
 function Card({data}) {
+    const [modal, setModal] = useState(false)
+    const [URL, setURL] = useState("")
+    const imageSRC = useRef(null)
+    useEffect(() => {
+        setURL(imageSRC.current.currentSrc)
+    }, [modal])
     return (
         <div className='card-post'>
-            <div className="card-post__img">
-                <img src={require(`../../../../../Backend/routes/images${data.poster}`).default} alt=""/>
+            <div className={`card-post__img `} onClick={()=>setModal(!modal)}>
+                <img ref={imageSRC} style={{cursor:"pointer"}} onClick={()=>setModal(!modal)} src={require(`../../../../../Backend/routes/images${data.poster}`).default} alt=""/>
             </div>
             <div className="card-post__info">
                 <div className="card-post__date">
@@ -19,6 +24,10 @@ function Card({data}) {
                 <p className='card-post__text' style={{fontSize:"1rem"}}>{data.deskripsi}</p>
                 <a className='card-post__cta' style={{cursor:"pointer"}} onClick={()=>window.location.href=`${data.link}`}>Read more</a>
             </div>    
+            <FsLightbox
+                toggler={modal}
+                sources={[URL]}
+            />
         </div>
     )
 }
